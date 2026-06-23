d19654ee187dccbe2d783b23c95f6cc3e6ed1875
SergiyOmelchuk <om.sergiy@gmail.com>
2026-06-23 21:10:18 +0300

    update frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,29 +3,40 @@ import { api } from './api';
 
 function App() {
   const [active, setActive] = useState(false);
+  const [name, setName] = useState(false);
+  const [inputName, setInputName] = useState('');
 
   async function loadState() {
     const res = await api.get('/toggle');
     setActive(res.data.active);
+      setName(res.data.name || '');
   }
 
   async function toggle() {
-    const res = await api.post('/toggle');
+    const res = await api.post('/toggle', { name: inputName });
     setActive(res.data.active);
+    setName(res.data.name || '');
+    setInputName('');
   }
 
   useEffect(() => {
-    loadState();
+    void loadState();
   }, []);
 
   return (
       <div>
         <h1>
-          Status: {active ? 'ON' : 'OFF'}
+            {`Status of ${name}: `}{active ? 'ON' : 'OFF'}
         </h1>
+          <input
+              type="text"
+              value={inputName}
+              onChange={(e) => setInputName(e.target.value)}
+              placeholder="Enter name"
+          />
 
         <button onClick={toggle}>
-          Toggle
+            Toggle
         </button>
       </div>
   );
